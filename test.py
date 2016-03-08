13caa88e7e25391ed7511b1b19787044c188b96c
elwali10 <walikarkoub@gmail.com>
2016-03-08 10:01:52 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import matplotlib
+
 from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
@@ -6,6 +7,8 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import tkinter as Tk
 
 root = Tk.Tk()
+
+
 
 fig = plt.figure(figsize=(12, 6))
 map = Basemap(llcrnrlon=-4.476,llcrnrlat=48.417,urcrnrlon=-4.469,urcrnrlat=48.42, epsg=2154)
